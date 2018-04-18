ab2dcc5c0169aad5fc5759405ff10040aae173a5
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2018-04-18 03:29:43 +0300

    Add bluebox

---
+++ bluebox/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.js-slider').slick({
+		dots: false,
+		infinite: true,
+		cssEase: 'linear',
+		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		mobileFirst: true,
+		// cssEase: true,
+	});
+});
+
